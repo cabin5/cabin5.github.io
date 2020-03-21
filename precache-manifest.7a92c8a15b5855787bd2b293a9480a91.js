5e930186d6414864b17b361dc65349267feda57c
cabin5 <cabin5@protonmail.com>
2020-03-21 17:08:08 -0400

    Updates

---
+++ precache-manifest.7a92c8a15b5855787bd2b293a9480a91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "098a5239dc28235e39cf9a9a82b7eb45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd43baf06881802560c9",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5b592613deddb63e10b3",
+    "url": "/static/js/2.8ba337b6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8ba337b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd43baf06881802560c9",
+    "url": "/static/js/main.1a84732b.chunk.js"
+  },
+  {
+    "revision": "8e81ec66b531d800c3fd",
+    "url": "/static/js/runtime-main.d0cbd169.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
